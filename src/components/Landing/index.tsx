828da798fb2b66826a2997af4dcc9da09c7f5565
saur0w <sthapliyal085@gmail.com>
2026-03-24 19:14:23 +0530

    "Styles and Landing"

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -3,14 +3,13 @@
 import { useRef } from "react";
 import { View } from "@react-three/drei";
 import LandingMesh from "@/components/Landing/Mesh";
+import styles from "@/components/style.module.css";
 
 export default function HeroSection() {
     const ref = useRef<HTMLDivElement>(null!);
 
     return (
-        <section style={{ height: "100vh" }}>
-            <div ref={ref} style={{ width: "100%", height: "100%" }} />
-
+        <section className={styles.page}>
             <View track={ref}>
                 <ambientLight intensity={0.5} />
                 <LandingMesh />
