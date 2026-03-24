af4674545b89e16cac35c3b190b596eb56f97571
saur0w <sthapliyal085@gmail.com>
2026-03-24 19:20:13 +0530

    "Des"

--- src/components/Des/index.tsx
+++ src/components/Des/index.tsx
@@ -1,27 +1,14 @@
 "use client";
 
-import { useEffect, useRef } from "react";
-import { useSceneStore } from "@/store/paths";
+import Mesh from "./Mesh";
+import {View} from "@react-three/drei";
 
 export default function Des() {
-    const ref = useRef<HTMLDivElement>(null);
-    const setActiveSection = useSceneStore((s) => s.setActiveSection);
-
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            ([entry]) => {
-                if (entry.isIntersecting) setActiveSection("des");
-            },
-            { threshold: 0.5 }
-        );
-
-        if (ref.current) observer.observe(ref.current);
-        return () => observer.disconnect();
-    }, [ setActiveSection]);
-
     return (
-        <div ref={ref} style={{ height: "100vh" }}>
-
+        <div>
+            <View>
+                <Mesh />
+            </View>
         </div>
-    );
+    )
 }
