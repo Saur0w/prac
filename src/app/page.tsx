e0a750bdf1f88ea3adcc9f95a1583c379cc05374
saur0w <sthapliyal085@gmail.com>
2026-03-24 19:28:33 +0530

    "Page"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 "use client";
+
 import styles from "./page.module.css";
+import Scene from "@/ui/Scene";
 import Landing from "@/components/Landing";
 import Des from "@/components/Des";
 import About from "@/components/About";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <Landing />
-      <Des />
-      <About />
-    </div>
-  );
+    return (
+        <div className={styles.page}>
+            <Landing />
+            <Des />
+            <About />
+            <Scene />
+        </div>
+    );
 }
